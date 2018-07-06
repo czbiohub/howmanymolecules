bccf62f1b8e8074b431657cbc16755220a7d574b
Sidney Bell <sidneymb@uw.edu>
2018-07-06 16:48:19 -0700

    Add n_molecules slider and pcr toggle

--- components/controls.js
+++ components/controls.js
@@ -6,7 +6,7 @@ function controls (opts) {
 	var box = document.createElement('div')
 
 	var state = {}
-	
+
 	css(box, {
 		backgroundColor: 'rgb(100,100,100)',
 		width: '170px',
@@ -20,12 +20,27 @@ function controls (opts) {
 	expression.type = 'range'
 	box.appendChild(expression)
 
+	var n_molecules = document.createElement('input')
+	n_molecules.type = 'range'
+	box.appendChild(n_molecules)
+
+	var pcr = document.createElement('input')
+	pcr.type = 'checkbox'
+	box.appendChild(pcr)
+
+	pcr.onchange = function (p) {
+	state['pcr'] = p.target.value
+	}
+	n_molecules.onchange = function (n) {
+		state['n_molecules'] = n.target.value
+	}
 	expression.onchange = function (e) {
 		state['expression'] = e.target.value
 	}
-
 	this.state = state
 	this.box = box
+
+	console.log('state', this.state)
 }
 
 module.exports = controls
